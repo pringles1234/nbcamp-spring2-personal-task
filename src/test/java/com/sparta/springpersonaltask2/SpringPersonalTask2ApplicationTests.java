b0c2a2064ff0ca3c05e597fa0e1bd758279d6a79
Yujin Lee <dbwls9040@gmail.com>
2024-08-26 22:14:14 +0900

    init commit

---
+++ src/test/java/com/sparta/springpersonaltask2/SpringPersonalTask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springpersonaltask2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPersonalTask2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
